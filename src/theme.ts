bc76c70091f23985de587a02305b55abb28bbebb
Uku Markus Tammet <ukutammet@gmail.com>
2020-03-24 19:04:27 +0200

    Add initial setup (from CRA) (#1)
    * Add initial setup (from CRA)
    
    * Remove leftovers
    
    * Update readme with npm instead of yarn
    
    * License under apache 2.0

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { system } from '@theme-ui/presets';
+
+const baseTheme = system;
+
+const theme = {
+  ...baseTheme,
+  buttons: {
+    primary: {
+      cursor: 'pointer',
+    },
+    secondary: {
+      cursor: 'pointer',
+    },
+  },
+};
+
+export default theme;
