f603ae7605449d0aab046770f983c992089f878e
Uku.Tammet <ukutammet@gmail.com>
2020-03-25 00:29:41 +0200

    Start working on initial prototype skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route, Link, useParams, useHistory } from 'react-router-dom';
 import {
   Input,
   Label,
@@ -10,14 +10,22 @@ import {
   Text,
   Heading,
   Container,
+  Spinner,
 } from 'theme-ui';
 import styled from '@emotion/styled';
 import { useFormik } from 'formik';
 import { useMediaLayout } from 'use-media';
+import * as yup from 'yup';
 
 import theme from './theme';
 
 const useMobileLayout = () => useMediaLayout({ maxWidth: '1000px' }); // TODO: how large should it be?
+
+const PageContainer = ({ children }: { children: React.ReactNode }) => (
+  <Container sx={{ maxWidth: '600px' }} py={4} px={2}>
+    {children}
+  </Container>
+);
 
 const BlockButton = styled(Button)(() => ({
   display: 'block',
@@ -26,66 +34,196 @@ const BlockButton = styled(Button)(() => ({
 
 const Homepage = () => {
   return (
-    <Container sx={{ maxWidth: '600px' }} py={4} px={2}>
+    <PageContainer>
       <Heading>COVID-19 clearing application</Heading>
       <Text my={2}>
-        This application is for authorized personnel to order and verify their results of the
-        COVID-19 antibody test, lorem ipsum etc
+        This application is for authorized folks to order and verify their results of the COVID-19
+        antibody test, lorem ipsum etc
       </Text>
       <Divider />
       <Grid gap={2}>
-        <BlockButton as={Link} to="/identity">
-          Enter your information
+        <BlockButton as={Link} to="/login">
+          Sign in
         </BlockButton>
       </Grid>
-    </Container>
+    </PageContainer>
   );
 };
 
 const IdentityPage = () => {
+  // TODO: get user id passed in by having the user-scoped pages as child pages of the main page
+  const { userId } = useParams();
+  const history = useHistory();
+
+  function handleIdentityFilled(identity: Identity) {
+    // TODO: call backend
+    history.push(`/users/${userId}/`);
+  }
+
   return (
-    <Container sx={{ maxWidth: '600px' }} py={4} px={2}>
-      <Heading mb={2}>Your identity, this is a test page to test react router</Heading>
-      <IdentityForm />
-    </Container>
+    <PageContainer>
+      <Heading mb={2}>Your details</Heading>
+      <IdentityForm onComplete={handleIdentityFilled} />
+    </PageContainer>
   );
 };
 
-const IdentityForm = () => {
+interface Identity {
+  name: string;
+  dateOfBirth: string;
+}
+
+const IdentityForm = ({ onComplete }: { onComplete: (identity: Identity) => any }) => {
   const form = useFormik({
     initialValues: {
       name: '',
       dateOfBirth: '',
     },
-    onSubmit(data) {
-      console.log(data);
-    },
+    validationSchema: yup.object().shape({
+      name: yup.string().required('Please fill your name'),
+      dateOfBirth: yup
+        .date()
+        .required('Please fill your date of birth')
+        .max(new Date(), 'Please check your date of birth'),
+    }),
+    onSubmit: onComplete,
   });
 
   return (
     <form onSubmit={form.handleSubmit}>
       <Label htmlFor="identity-name">Your full name</Label>
-      <Input
-        name="name"
-        id="identity-name"
-        type="text"
-        onChange={form.handleChange}
-        value={form.values.name}
-        mb={2}
-      />
-      <Label htmlFor="identity-dateOfBirth">Your date of birth</Label>
+      <Input name="name" id="identity-name" type="text" {...form.getFieldProps('name')} />
+      {form.touched.name && form.errors.name ? <Text>{form.errors.name}</Text> : null}
+      <Label mt={2} htmlFor="identity-dateOfBirth">
+        Your date of birth
+      </Label>
       <Input
         name="dateOfBirth"
         id="identity-dateOfBirth"
         type="date"
-        onChange={form.handleChange}
-        value={form.values.dateOfBirth}
-        mb={2}
+        {...form.getFieldProps('dateOfBirth')}
       />
-      <BlockButton type="submit">Submit</BlockButton>
+      {form.touched.dateOfBirth && form.errors.dateOfBirth ? (
+        <Text>{form.errors.dateOfBirth}</Text>
+      ) : null}
+      <BlockButton mt={2} type="submit" disabled={form.isSubmitting}>
+        Submit
+      </BlockButton>
     </form>
   );
 };
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const submitted = !!email;
+
+  function handleLogin({ email }: { email: string }) {
+    // TODO: call backend
+    setEmail(email);
+  }
+
+  return (
+    <PageContainer>
+      <Heading mb={2}>Sign in</Heading>
+      {submitted ? (
+        <Text>
+          Please check your inbox at <strong>{email}</strong> for the signup link.
+        </Text>
+      ) : (
+        <LoginForm onComplete={handleLogin} />
+      )}
+    </PageContainer>
+  );
+};
+
+const LoginForm = ({ onComplete }: { onComplete: ({ email }: { email: string }) => any }) => {
+  const form = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: yup.object().shape({
+      email: yup
+        .string()
+        .email('Please check your email address')
+        .required('Please fill your email address'),
+    }),
+    onSubmit: onComplete,
+  });
+
+  return (
+    <form onSubmit={form.handleSubmit}>
+      <Label htmlFor="cov-email">Your email</Label>
+      <Input name="email" id="cov-email" type="email" {...form.getFieldProps('email')} />
+      {form.touched.email && form.errors.email ? <Text>{form.errors.email}</Text> : null}
+      <BlockButton type="submit" disabled={form.isSubmitting} mt={2}>
+        Submit
+      </BlockButton>
+    </form>
+  );
+};
+
+const LinkPage = () => {
+  const history = useHistory();
+  const { linkId } = useParams();
+  useEffect(() => {
+    // TODO: api call here
+    const timeout = setTimeout(() => {
+      // TODO: replace with user id gotten through auth
+      // TODO: do not redirect /identity if no identity created, but to main page instead
+      history.replace('/users/123/identity');
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [history, linkId]);
+  return (
+    <PageContainer>
+      <Spinner mx="auto" sx={{ display: 'block' }} />
+    </PageContainer>
+  );
+};
+
+const MainPage = () => {
+  const { userId } = useParams();
+  // TODO: if the user does not have identity, redirect to /identity
+  return (
+    <PageContainer>
+      <Heading>
+        I will show your tests and redirect to qr code page if no tests yet, and have an option to
+        show the code if you have tests
+      </Heading>
+    </PageContainer>
+  );
+};
+
+const CodePage = () => {
+  return (
+    <PageContainer>
+      <Heading>I will show the QR code</Heading>
+    </PageContainer>
+  );
+};
+
+const ScanPage = () => {
+  return (
+    <PageContainer>
+      <Heading>I will show the QR code</Heading>
+    </PageContainer>
+  );
+};
+
+/*
+homepage ->
+login ->
+click on email magic link ->
+link/:uuid/ -redirect->
+/user/:id/identity ->
+/user/:id/ -> Main page. Shows user's tests if they exist.
+If they are a tester/checker, has a "scan" button, and an "add test" button.
+If they are not, shows "show code" button. If they are not and no tests have been done, shows code immediately.
+/user/:id/code -> shows qr code (this will prolly be a modal)
+
+/scan -> scans qr codes to do checker/tester flow
+*/
 
 const App = () => {
   return (
@@ -95,8 +233,23 @@ const App = () => {
           <Route path="/" exact>
             <Homepage />
           </Route>
-          <Route path="/identity" exact>
+          <Route path="/login" exact>
+            <LoginPage />
+          </Route>
+          <Route path="/link/:linkId" exact>
+            <LinkPage />
+          </Route>
+          <Route path="/users/:userId/identity" exact>
             <IdentityPage />
+          </Route>
+          <Route path="/users/:userId/" exact>
+            <MainPage />
+          </Route>
+          <Route path="/users/:userId/code" exact>
+            <CodePage />
+          </Route>
+          <Route path="/scan" exact>
+            <ScanPage />
           </Route>
         </Switch>
       </BrowserRouter>
