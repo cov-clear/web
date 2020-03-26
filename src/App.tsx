532c824077ce3299b5183a0be1d14445471a0ff9
Uku.Tammet <ukutammet@gmail.com>
2020-03-27 00:24:11 +0200

    Integrate login page into the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import {
-  Input,
-  Label,
-  Button,
-  Grid,
-  ThemeProvider,
-  Divider,
-  Text,
-  Heading,
-  Container,
-} from 'theme-ui';
-import styled from '@emotion/styled';
-import { useFormik } from 'formik';
-import { useMediaLayout } from 'use-media';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ThemeProvider, Container, Heading } from 'theme-ui';
 
+import { LoginPage } from './login';
 import theme from './theme';
-
-const useMobileLayout = () => useMediaLayout({ maxWidth: '1000px' }); // TODO: how large should it be?
-
-const BlockButton = styled(Button)(() => ({
-  display: 'block',
-  width: '100%',
-})) as any;
-
-const Homepage = () => {
-  return (
-    <Container sx={{ maxWidth: '600px' }} py={4} px={2}>
-      <Heading>COVID-19 clearing application</Heading>
-      <Text my={2}>
-        This application is for authorized personnel to order and verify their results of the
-        COVID-19 antibody test, lorem ipsum etc
-      </Text>
-      <Divider />
-      <Grid gap={2}>
-        <BlockButton as={Link} to="/identity">
-          Enter your information
-        </BlockButton>
-      </Grid>
-    </Container>
-  );
-};
-
-const IdentityPage = () => {
-  return (
-    <Container sx={{ maxWidth: '600px' }} py={4} px={2}>
-      <Heading mb={2}>Your identity, this is a test page to test react router</Heading>
-      <IdentityForm />
-    </Container>
-  );
-};
-
-const IdentityForm = () => {
-  const form = useFormik({
-    initialValues: {
-      name: '',
-      dateOfBirth: '',
-    },
-    onSubmit(data) {
-      console.log(data);
-    },
-  });
-
-  return (
-    <form onSubmit={form.handleSubmit}>
-      <Label htmlFor="identity-name">Your full name</Label>
-      <Input
-        name="name"
-        id="identity-name"
-        type="text"
-        onChange={form.handleChange}
-        value={form.values.name}
-        mb={2}
-      />
-      <Label htmlFor="identity-dateOfBirth">Your date of birth</Label>
-      <Input
-        name="dateOfBirth"
-        id="identity-dateOfBirth"
-        type="date"
-        onChange={form.handleChange}
-        value={form.values.dateOfBirth}
-        mb={2}
-      />
-      <BlockButton type="submit">Submit</BlockButton>
-    </form>
-  );
-};
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Switch>
-          <Route path="/" exact>
-            <Homepage />
-          </Route>
-          <Route path="/identity" exact>
-            <IdentityPage />
-          </Route>
-        </Switch>
+        <Container sx={{ maxWidth: '600px' }} pt={5} pb={4} px={2}>
+          <Switch>
+            <Route path="/login" exact>
+              <LoginPage />
+            </Route>
+            <Route path="*">
+              <Heading>Not found</Heading>
+            </Route>
+          </Switch>
+        </Container>
       </BrowserRouter>
     </ThemeProvider>
   );
