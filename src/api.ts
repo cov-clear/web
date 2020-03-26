93df0ce8027a30ac7e4f5638111aa3685f4208ea
Steve Pole <StevePole@users.noreply.github.com>
2020-03-26 20:17:54 +0000

    Fix up issues in API client

--- src/api.ts
+++ src/api.ts
@@ -1,45 +1,45 @@
 interface Profile {
-  name: string,
-  dateOfBirth: string,
-  sex: string,
+  name: string;
+  dateOfBirth: string;
+  sex: string;
 }
 
 interface Address {
-  address1: string,
-  address2?: string,
-  city: string,
-  region: string,
-  postcode: string,
-  countryCode: string
+  address1: string;
+  address2?: string;
+  city: string;
+  region: string;
+  postcode: string;
+  countryCode: string;
 }
 
 interface User {
-  id?: string,
-  email: string,
-  roles: string, // Define this
-  profile: Profile,
-  address: Address
+  id?: string;
+  email: string;
+  roles: string; // Define this
+  profile: Profile;
+  address: Address;
 }
 
 interface TestType {
-  id: string,
-  name: string
-  resultsSchema: object,
-  requiresTrustedTester: boolean
+  id: string;
+  name: string;
+  resultsSchema: object;
+  requiresTrustedTester: boolean;
 }
 
 interface TestResult {
-  id?: string,
-  testType: string,
-  userId: string,
-  results: object,
-  testerId: string,
-  trustedTester?: boolean
+  id?: string;
+  testType: string;
+  userId: string;
+  results: object;
+  testerId: string;
+  trustedTester?: boolean;
 }
 
 interface SharingCode {
-  code: string,
-  expiry: string
+  code: string;
+  expiry: string;
 }
 
 const user = {
@@ -65,6 +65,7 @@ const testResult = {
   id: "ABCDE-12345-ABCDE-12346",
   testType: "Antibody",
   userId: "ABCDE-12345-ABCDE-12345",
+  dateTaken: "2020-04-10",
   results: {
     c: true,
     igg: true,
@@ -80,24 +81,23 @@ const testType = {
   resultsSchema: {
     type: "object",
     properties: {
-      c: { type: boolean, title: "C" },
-      igg: { type: boolean, title: "IgG" },
-      igm: { type: boolean, title: "IgM" },
+      c: { type: "boolean", title: "C" },
+      igg: { type: "boolean", title: "IgG" },
+      igm: { type: "boolean", title: "IgM" }
     }
   },
   requiresTrustedTester: false
-}
+};
 
 const sharingCode = {
   code: "3a44e5ce-a799-4fe8-a626-cc2e05deaf3a",
   expiry: "2020-04-01T00:00:00Z"
 };
 
-
 function createMagicLink(email: string): Promise<undefined> {
   // POST /auth/magic-links { email }
   return getPromise();
-};
+}
 
 function createLogin(email: string, authCode: string): Promise<undefined> {
   // POST /auth/login { email, authCode, method: "magic-link" }
@@ -120,32 +120,46 @@ function updateUser(token: string, userId: string, user: User): Promise<User> {
   return getPromise(user);
 }
 
-function getTestResults(token: string, userId: string): Promise<Array<TestResult>> {
+function getTestResults(
+  token: string,
+  userId: string
+): Promise<Array<TestResult>> {
   // GET /users/:userId/tests
   return getPromise([testResult]);
 }
 
-function createTestResult(token: string, userId: string, data): Promise<TestResult> {
+function createTestResult(
+  token: string,
+  userId: string,
+  data: any
+): Promise<TestResult> {
   // POST /users/:userId/tests { ...data }
   return getPromise(testResult);
 }
 
-function createSharingCode(token: string, userId: string): Promise<SharingCode> {
+function createSharingCode(
+  token: string,
+  userId: string
+): Promise<SharingCode> {
   // POST /users/:userId/sharing-code { }
   return getPromise(sharingCode);
 }
 
-function requestAccess(token: string, userId: string, sharingCode: string): Promise<object> {
+function requestAccess(
+  token: string,
+  userId: string,
+  sharingCode: string
+): Promise<object> {
   // POST /users/:userId/access-requests { sharingCode }
   return getPromise({ userId: "ABCDE-12345-ABCDE-12345" });
 }
 
-function getTestTypes(token: string)Promise<Array<TestType>> {
+function getTestTypes(token: string): Promise<Array<TestType>> {
   // GET /test-types
   return getPromise([testType]);
 }
 
-function getPromise(response: any): Promise<any> {
+function getPromise(response?: any): Promise<any> {
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve(response), 1000);
   });
