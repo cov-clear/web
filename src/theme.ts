9805ed197492924d823391b46f3f7aac311150a8
Uku.Tammet <ukutammet@gmail.com>
2020-03-29 01:49:37 +0200

    Update primary color

--- src/theme.ts
+++ src/theme.ts
@@ -5,6 +5,10 @@ const baseTheme = system;
 const theme = {
   ...baseTheme,
   space: [0, 4, 8, 16, 24, 40, 64, 128, 256, 512],
+  colors: {
+    ...baseTheme.colors,
+    primary: '#1890FF',
+  },
   buttons: {
     primary: {
       cursor: 'pointer',
