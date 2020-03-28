533e64d5ae5cdade2e9acc42a788cc029910da74
Uku.Tammet <ukutammet@gmail.com>
2020-03-28 12:20:45 +0200

    Wire up link page

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,39 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider, Container, Heading } from 'theme-ui';
 
-import { LoginPage } from './login';
+import { LoginPage, LinkPage, Provider as AuthenticationProvider } from './authentication';
 import theme from './theme';
+
+const NotFound = () => <Heading>Not found</Heading>;
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Container sx={{ maxWidth: '600px' }} pt={5} pb={4} px={2}>
-          <Switch>
-            <Route path="/login" exact>
-              <LoginPage />
-            </Route>
-            <Route path="*">
-              <Heading>Not found</Heading>
-            </Route>
-          </Switch>
-        </Container>
-      </BrowserRouter>
-    </ThemeProvider>
+    <Container sx={{ maxWidth: '600px' }} pt={5} pb={4} px={2}>
+      <Switch>
+        <Route path="/login" exact>
+          <LoginPage />
+        </Route>
+        <Route path="/link/:linkId" exact>
+          <LinkPage />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Container>
   );
 };
 
-export default App;
+const ConfiguredApp = () => {
+  return (
+    <AuthenticationProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthenticationProvider>
+  );
+};
+
+export default ConfiguredApp;
