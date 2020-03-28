b623c8f8356c3bf564c34220448fd0c68b93035e
Uku.Tammet <ukutammet@gmail.com>
2020-03-29 01:35:59 +0200

    Clean up signup experience

--- src/authentication/LoginPage.test.tsx
+++ src/authentication/LoginPage.test.tsx
@@ -21,36 +21,36 @@ describe('Login page', () => {
 
   it('creates a magic link to your email', async () => {
     expect(createMagicLink).not.toHaveBeenCalled();
-    expect(screen.queryByText(/check your inbox/)).toBeNull();
+    expect(screen.queryByText(/we sent an email/i)).toBeNull();
 
     fireEvent.change(screen.getByLabelText(/email/i), { target: { value: 'person@example.com' } });
-    fireEvent.click(screen.getByText(/submit/i));
-    expect(await screen.findByText(/check your inbox/)).not.toBeNull();
+    fireEvent.click(screen.getByText(/send magic link/i));
+    expect(await screen.findByText(/we sent an email/i)).not.toBeNull();
     expect(createMagicLink).toHaveBeenCalledWith('person@example.com');
   });
 
   it('does not let you input an invalid email', async () => {
     expect(screen.queryByText(/check your email/i)).toBeNull();
     fireEvent.change(screen.getByLabelText(/email/i), { target: { value: 'person-example.com' } });
-    fireEvent.click(screen.getByText(/submit/i));
+    fireEvent.click(screen.getByText(/send magic link/i));
     expect(await screen.findByText(/check your email/i)).not.toBeNull();
     expect(createMagicLink).not.toHaveBeenCalled();
   });
 
   it('does not let you sign in with a missing email', async () => {
     expect(screen.queryByText(/fill your email/i)).toBeNull();
-    fireEvent.click(screen.getByText(/submit/i));
+    fireEvent.click(screen.getByText(/send magic link/i));
     expect(await screen.findByText(/fill your email/i)).not.toBeNull();
     expect(createMagicLink).not.toHaveBeenCalled();
   });
 
   it('does not let you sign in multiple times while loading', async () => {
     fireEvent.change(screen.getByLabelText(/email/i), { target: { value: 'person@example.com' } });
-    fireEvent.click(screen.getByText(/submit/i));
-    fireEvent.click(screen.getByText(/submit/i));
-    fireEvent.click(screen.getByText(/submit/i));
-    fireEvent.click(screen.getByText(/submit/i));
-    expect(await screen.findByText(/check your inbox/)).not.toBeNull();
+    fireEvent.click(screen.getByText(/send magic link/i));
+    fireEvent.click(screen.getByText(/send magic link/i));
+    fireEvent.click(screen.getByText(/send magic link/i));
+    fireEvent.click(screen.getByText(/send magic link/i));
+    expect(await screen.findByText(/we sent an email/i)).not.toBeNull();
     expect(createMagicLink).toHaveBeenCalledWith('person@example.com');
     expect(createMagicLink).toHaveBeenCalledTimes(1);
   });
