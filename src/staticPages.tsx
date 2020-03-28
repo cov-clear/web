bda3e18eb8b7dc58cb18382c66da0abca0df3d54
Uku.Tammet <ukutammet@gmail.com>
2020-03-28 14:41:51 +0200

    Add some illustrations

--- src/staticPages.tsx
+++ src/staticPages.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 import { Heading, Button, ButtonProps, Image } from 'theme-ui';
 
-import medicalIllustration from './medicalIllustration.svg';
+import medicalIllustration from './illustrations/medicalIllustration.svg';
+import balloonIllustration from './illustrations/balloons.svg';
 
 const NavButton = Button as React.FC<ButtonProps & LinkProps>;
 export const NotFoundPage = () => (
   <>
     <Heading mb={4}>This page wasn't found</Heading>
+    <Image src={balloonIllustration} alt="A woman flying away with a bundle of balloons" mb={2} />
     <NavButton as={Link} sx={{ display: 'block', width: '100%' }} to="/">
       Go to the start page
     </NavButton>
