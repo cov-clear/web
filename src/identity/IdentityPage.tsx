7c318cd1a006a256039f2a11a25c36483faed692
Uku.Tammet <ukutammet@gmail.com>
2020-03-28 23:58:50 +0200

    Refactor profile form

--- src/identity/IdentityPage.tsx
+++ src/identity/IdentityPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import { Spinner, Label, Input, Button, Text, Heading, Radio } from 'theme-ui';
+import { Spinner, Text, Heading } from 'theme-ui';
 import { useParams } from 'react-router-dom';
 import { format } from 'date-fns';
 
 import { useUser } from '../resources';
-import { Profile, Sex } from '../api';
+import { Profile } from '../api';
+
+import { ProfileForm } from './ProfileForm';
 
 export const IdentityPage = () => {
   const { userId } = useParams<{ userId: string }>();
@@ -17,10 +17,7 @@ export const IdentityPage = () => {
   }
 
   function createProfile(profile: Profile) {
-    if (user) {
-      return updateUser({ ...user, profile });
-    }
-    return Promise.reject();
+    return updateUser({ ...user!, profile });
   }
 
   return (
@@ -50,76 +47,3 @@ const Identity = ({ email, profile }: { email: string; profile: Profile }) => {
   );
 };
 
-const ProfileForm = ({ onComplete }: { onComplete: (profile: Profile) => Promise<any> }) => {
-  const form = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      dateOfBirth: '',
-      sex: '',
-    },
-    validationSchema: yup.object().shape({
-      firstName: yup
-        .string()
-        .trim()
-        .required('Please fill your first name(s)'),
-      lastName: yup
-        .string()
-        .trim()
-        .required('Please fill your last name'),
-      dateOfBirth: yup
-        .date()
-        .required('Please fill your date of birth')
-        .max(new Date(), 'Please check your date of birth'),
-      sex: yup
-        .string()
-        .oneOf([Sex.MALE, Sex.FEMALE])
-        .required('Please select your legal sex'),
-    }),
-    onSubmit: ({ firstName, lastName, dateOfBirth, sex }) =>
-      onComplete({
-        firstName: firstName.trim(),
-        lastName: lastName.trim(),
-        dateOfBirth,
-        sex: sex as Sex,
-      }),
-  });
-
-  return (
-    <form onSubmit={form.handleSubmit}>
-      <Label htmlFor="identity-first-name">Your first name(s)</Label>
-      <Input id="identity-first-name" type="text" {...form.getFieldProps('firstName')} />
-      {form.touched.firstName && form.errors.firstName ? (
-        <Text>{form.errors.firstName}</Text>
-      ) : null}
-
-      <Label mt={2} htmlFor="identity-last-name">
-        Your last name
-      </Label>
-      <Input name="name" id="identity-last-name" type="text" {...form.getFieldProps('lastName')} />
-      {form.touched.lastName && form.errors.lastName ? <Text>{form.errors.lastName}</Text> : null}
-
-      <Label mt={2} htmlFor="identity-dateOfBirth">
-        Your date of birth
-      </Label>
-      <Input id="identity-dateOfBirth" type="date" {...form.getFieldProps('dateOfBirth')} />
-      {form.touched.dateOfBirth && form.errors.dateOfBirth ? (
-        <Text>{form.errors.dateOfBirth}</Text>
-      ) : null}
-
-      <Label mt={2}>
-        <Radio {...form.getFieldProps({ name: 'sex', type: 'radio', value: Sex.FEMALE })} />
-        Female
-      </Label>
-      <Label>
-        <Radio {...form.getFieldProps({ name: 'sex', type: 'radio', value: Sex.MALE })} />
-        Male
-      </Label>
-      {form.touched.sex && form.errors.sex ? <Text>{form.errors.sex}</Text> : null}
-
-      <Button mt={2} variant="block" type="submit" disabled={form.isSubmitting}>
-        Submit
-      </Button>
-    </form>
-  );
-};
---
+++ src/identity/ProfileForm.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { Box, Radio, Label, Input, Button, Text } from 'theme-ui';
+
+import { Profile, Sex } from '../api';
+
+const AnyBox = Box as any;
+
+interface ProfileFormFields {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  sex: Sex | '';
+}
+
+export const ProfileForm = ({ onComplete }: { onComplete: (profile: Profile) => Promise<any> }) => {
+  const form = useFormik<ProfileFormFields>({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      dateOfBirth: '',
+      sex: '',
+    },
+    validationSchema: yup.object().shape({
+      firstName: yup
+        .string()
+        .trim()
+        .required('Please fill your first name(s)'),
+      lastName: yup
+        .string()
+        .trim()
+        .required('Please fill your last name'),
+      dateOfBirth: yup
+        .date()
+        .required('Please fill your date of birth')
+        .max(new Date(), 'Please check your date of birth'),
+      sex: yup
+        .string()
+        .oneOf([Sex.MALE, Sex.FEMALE])
+        .required('Please select your legal sex'),
+    }),
+    onSubmit: ({ firstName, lastName, dateOfBirth, sex }) =>
+      onComplete({
+        firstName: firstName.trim(),
+        lastName: lastName.trim(),
+        dateOfBirth,
+        sex: sex as Sex,
+      }),
+  });
+
+  const fieldError = (key: keyof ProfileFormFields) =>
+    form.touched[key] && form.errors[key] ? <Text>{form.errors[key]}</Text> : null;
+
+  return (
+    <AnyBox as="form" sx={{ display: 'grid', gridGap: 2 }} onSubmit={form.handleSubmit}>
+      <Box>
+        <Label htmlFor="identity-first-name">First names</Label>
+        <Input id="identity-first-name" type="text" {...form.getFieldProps('firstName')} />
+        {fieldError('firstName')}
+      </Box>
+
+      <Box>
+        <Label htmlFor="identity-last-name">Last names</Label>
+        <Input
+          name="name"
+          id="identity-last-name"
+          type="text"
+          {...form.getFieldProps('lastName')}
+        />
+        {fieldError('lastName')}
+      </Box>
+
+      <Box>
+        <Label htmlFor="identity-dateOfBirth">Your date of birth</Label>
+        <Input id="identity-dateOfBirth" type="date" {...form.getFieldProps('dateOfBirth')} />
+        {fieldError('dateOfBirth')}
+      </Box>
+
+      <Box>
+        <Label>
+          <Radio {...form.getFieldProps({ name: 'sex', type: 'radio', value: Sex.FEMALE })} />
+          Female
+        </Label>
+        <Label>
+          <Radio {...form.getFieldProps({ name: 'sex', type: 'radio', value: Sex.MALE })} />
+          Male
+        </Label>
+        {fieldError('sex')}
+      </Box>
+
+      <Button variant="block" type="submit" disabled={form.isSubmitting}>
+        Submit
+      </Button>
+    </AnyBox>
+  );
+};
