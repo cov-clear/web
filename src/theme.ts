9f377de3698251fbb941cad151259f04e626c604
Uku.Tammet <ukutammet@gmail.com>
2020-03-29 18:11:34 +0300

    Add tab styling and iterate on design

--- src/theme.ts
+++ src/theme.ts
@@ -2,19 +2,45 @@ import { system } from '@theme-ui/presets';
 
 const baseTheme = system;
 
+const normalWeight = 400;
+const semibold = 600;
+
 const theme = {
   ...baseTheme,
   space: [0, 4, 8, 16, 24, 40, 64, 128, 256, 512],
   colors: {
     ...baseTheme.colors,
-    primary: '#1890FF',
+    // primary: '#1890FF',
+    primary: '#096DD9',
+  },
+  links: {
+    tab: {
+      textAlign: 'center',
+      borderBottom: '1px solid',
+      borderColor: '#DEDEDE', // TODO: grab this from the design system
+      display: 'block',
+      width: '100%',
+      padding: 2,
+      fontWeight: normalWeight,
+      '&.active': {
+        fill: 'primary',
+        borderBottom: '4px solid',
+        borderColor: 'primary',
+        fontWeight: semibold,
+      },
+      '&:hover': {
+        fill: 'primary',
+      },
+    },
   },
   buttons: {
     primary: {
       cursor: 'pointer',
+      fontWeight: semibold,
     },
     block: {
       cursor: 'pointer',
+      fontWeight: semibold,
       display: 'block',
       width: '100%',
     },
