0eb6ffcd52253e6f50c7785f72132bc1bf0873b8
Uku.Tammet <ukutammet@gmail.com>
2020-03-31 21:18:09 +0300

    Add proper view when looking at another person's user

--- src/identity/IdentityPage.tsx
+++ src/identity/IdentityPage.tsx
@@ -30,6 +30,7 @@ import { ProfileForm } from './ProfileForm';
 import { AddressForm } from './AddressForm';
 import { SharingCode } from './SharingCode';
 import { Test as TestIcon, Profile as ProfileIcon, Camera as CameraIcon } from './icons';
+import { useAuthentication } from '../authentication';
 
 const Small = ({ children }: { children: React.ReactNode }) => (
   <Text as="small" sx={{ fontSize: 2, fontWeight: 2 }}>
@@ -46,6 +47,8 @@ export const IdentityPage = () => {
     params: { userId },
   } = useRouteMatch();
   const { user, update: updateUser } = useUser(userId);
+  const { userId: authenticatedUserId } = useAuthentication();
+  const isOwnUser = userId === authenticatedUserId;
 
   if (!user) {
     return <Spinner variant="spinner.main" />;
@@ -82,33 +85,58 @@ export const IdentityPage = () => {
   }
 
   return (
-    <Container variant="page">
-      <Heading as="h1" mb={5}>
-        {user.profile.firstName} {user.profile.lastName}
-      </Heading>
-      <Text mb={4}>Date of birth: {format(new Date(user.profile.dateOfBirth), 'dd/MM/yyyy')}</Text>
-      <Flex as="nav">
-        <NavLink as={RouterNavLink} to={`${url}/profile`} variant="tab">
-          <ProfileIcon /> Profile
-        </NavLink>
-        <NavLink as={RouterNavLink} to={`${url}/tests`} variant="tab">
-          <TestIcon /> Tests
-        </NavLink>
-      </Flex>
-      <Switch>
-        <Route path={`${url}/profile`} exact>
-          <Box mt={6}>
-            <SharingCode userId={userId} />
-          </Box>
-        </Route>
-        <Route path={`${url}/tests`} exact>
-          Tests will be shown here
-        </Route>
-      </Switch>
-      <Redirect from={url} to={`${url}/profile`} />
-      <LinkButton as={Link} to="/scan" variant="fab">
-        <CameraIcon /> Scan
-      </LinkButton>
-    </Container>
+    <>
+      {!isOwnUser ? <ViewingOtherProfileHeader /> : null}
+      <Container variant="page" pt={isOwnUser ? undefined : 4}>
+        <Heading as="h1" mb={5}>
+          {user.profile.firstName} {user.profile.lastName}
+        </Heading>
+        <Text mb={4}>
+          Date of birth: {format(new Date(user.profile.dateOfBirth), 'dd/MM/yyyy')}
+        </Text>
+        {isOwnUser ? (
+          <Flex as="nav">
+            <NavLink as={RouterNavLink} to={`${url}/profile`} variant="tab">
+              <ProfileIcon /> Profile
+            </NavLink>
+            <NavLink as={RouterNavLink} to={`${url}/tests`} variant="tab">
+              <TestIcon /> Tests
+            </NavLink>
+          </Flex>
+        ) : null}
+        <Switch>
+          <Route path={`${url}/profile`} exact>
+            <Box mt={6}>
+              <SharingCode userId={userId} />
+            </Box>
+          </Route>
+          <Route path={`${url}/tests`} exact>
+            Tests will be shown here
+          </Route>
+        </Switch>
+        {isOwnUser ? (
+          <Redirect from={url} to={`${url}/profile`} />
+        ) : (
+          <Redirect from={url} to={`${url}/tests`} />
+        )}
+        <LinkButton as={Link} to="/scan" variant="fab">
+          <CameraIcon /> Scan
+        </LinkButton>
+      </Container>
+    </>
   );
 };
+
+// TODO: the styling on this header is very ad-hoc. Let's move it into the theme.
+const ViewingOtherProfileHeader = () => (
+  <Box sx={{ backgroundColor: 'primary', color: 'background' }}>
+    <Flex px={3} py={2} sx={{ justifyContent: 'space-between', maxWidth: '600px' }} mx="auto">
+      <Heading as="h2" sx={{ lineHeight: 1.8 }}>
+        Viewing profile
+      </Heading>
+      <LinkButton as={Link} to="/scan" sx={{ border: '2px solid', borderColor: 'background' }}>
+        Close
+      </LinkButton>
+    </Flex>
+  </Box>
+);
