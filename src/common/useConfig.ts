cdb2e3d68eb241458e0e9f7effd66c2a344f338a
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2020-05-04 13:47:44 +0300

    Add Estonian translations with temporary config mimicking solution

--- src/common/useConfig.ts
+++ src/common/useConfig.ts
@@ -3,7 +3,14 @@ import { Config, AuthenticationMethod, Language } from '../api';
 export function useConfig(): Config {
   // TODO: Get from API and store in context
   return {
-    authenticationMethod: AuthenticationMethod.ESTONIAN_ID,
-    defaultLanguage: Language.ESTONIAN,
+    authenticationMethod: isEstonianDeployment()
+      ? AuthenticationMethod.ESTONIAN_ID
+      : AuthenticationMethod.MAGIC_LINK,
+    defaultLanguage: isEstonianDeployment() ? Language.ESTONIAN : Language.ENGLISH,
   };
+}
+
+// TODO: Remove once we get the config from the API
+function isEstonianDeployment(): boolean {
+  return window.location.hostname.split('.')[0] === 'ee';
 }
--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -1,5 +1,7 @@
 import en from './en.json';
+import et from './et.json';
 
 export const messages = {
   en,
+  et,
 };
