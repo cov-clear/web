4ed09ddcf7ac6e6dce14347a4916a370ff17586e
Uku.Tammet <ukutammet@gmail.com>
2020-05-09 19:18:16 +0300

    Fix typings

--- src/common/useI18n.ts
+++ src/common/useI18n.ts
@@ -3,13 +3,11 @@ import { formatDate } from '../i18n/date';
 
 export function useI18n(): { formatDate: (date: Date) => string } {
   const config = useConfig();
-
   if (config) {
     return {
       formatDate: (date) => formatDate(date, config.defaultLanguage),
     };
   }
-
   return {
     formatDate: (date) => formatDate(date),
   };
