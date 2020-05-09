13b7e71f582cb18d32a9890f030f3827534b1a1e
Uku.Tammet <ukutammet@gmail.com>
2020-05-09 12:14:44 +0300

    Add tests for new scanpage functionality

--- src/scanning/ScanPage.test.tsx
+++ src/scanning/ScanPage.test.tsx
@@ -7,17 +7,15 @@ import { createMemoryHistory, History } from 'history';
 
 import { ScanPage } from './ScanPage';
 
-import { createAccessPass } from '../api';
 import { useAuthentication } from '../authentication';
 import { renderWrapped } from '../testHelpers';
+import nock from 'nock';
 
-jest.mock('../api');
 jest.mock('../authentication');
 jest.mock('react-qr-reader', () => jest.fn());
 jest.useFakeTimers();
 
 const useAuthenticationMock = useAuthentication as jest.MockedFunction<typeof useAuthentication>;
-const createAccessPassMock = createAccessPass as jest.MockedFunction<typeof createAccessPass>;
 
 const mockToken = 'mock-token';
 const mockUser = 'mock-user';
@@ -29,15 +27,12 @@ describe('Identity page', () => {
 
   beforeEach(() => {
     history = createMemoryHistory();
-    createAccessPassMock.mockImplementation(async () => ({
-      userId: 'mock-patient-id',
-      expiryTime: new Date().toISOString(),
-    }));
     useAuthenticationMock.mockImplementation(() => ({
       token: mockToken,
       userId: mockUser,
       authenticate: jest.fn(),
       signOut: jest.fn(),
+      hasPermission: () => true,
     }));
     (QRReader as any).mockImplementation(({ onScan }: { onScan: (data: string | null) => any }) => {
       mockScan = (data: string | null) => act(() => onScan(data));
@@ -52,24 +47,35 @@ describe('Identity page', () => {
   });
 
   it('scans a sharing code, creates and uses a sharing pass for it', async () => {
+    nock(/./)
+      .post('/api/v1/users/mock-user/access-passes', { code: mockSharingCode })
+      .matchHeader('Authorization', `Bearer ${mockToken}`)
+      .reply(200, { userId: 'mock-patient-id', expiryTime: new Date().toISOString() });
     expect(history.location.pathname).not.toBe('/users/mock-user');
     await mockScan(mockSharingCode);
     expect(history.location.pathname).toBe('/users/mock-patient-id');
-    expect(createAccessPassMock).toHaveBeenCalledTimes(1);
-    expect(createAccessPassMock).toHaveBeenCalledWith(mockUser, mockSharingCode, {
-      token: mockToken,
-    });
   });
 
   it('does not try to create access passes for invalid sharing codes', async () => {
+    const scope = nock(/./).post('/api/v1/users/mock-user/access-passes').reply(500);
     await mockScan('wrong-data');
     await mockScan(null);
-    expect(createAccessPassMock).not.toHaveBeenCalled();
+    expect(scope.isDone()).toBe(false);
   });
 
   it('shows errors for incorrect codes', async () => {
     expect(screen.queryByText(/incorrect/i)).not.toBeTruthy();
     await mockScan('wrong-data');
+    expect(screen.queryByText(/incorrect/i)).toBeTruthy();
+  });
+
+  it('shows errors when backend fails to create an access pass', async () => {
+    nock(/./)
+      .post('/api/v1/users/mock-user/access-passes', { code: mockSharingCode })
+      .matchHeader('Authorization', `Bearer ${mockToken}`)
+      .reply(400);
+    expect(screen.queryByText(/incorrect/i)).not.toBeTruthy();
+    await mockScan(mockSharingCode);
     expect(screen.queryByText(/incorrect/i)).toBeTruthy();
   });
 
