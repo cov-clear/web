fbc5e43ef77e3041524bbff1c5be65fa1c71e525
Uku.Tammet <ukutammet@gmail.com>
2020-05-09 17:25:33 +0300

    Add translation check to development

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -1,7 +1,20 @@
 import en from './en.json';
 import et from './et.json';
 
+if (process.env.NODE_ENV !== 'production') {
+  checkAllKeysMatch(en, et);
+  checkAllKeysMatch(et, en);
+}
+
 export const messages = {
   en,
   et,
 };
+
+function checkAllKeysMatch(a: Object, b: Object) {
+  Object.keys(a).forEach((key) => {
+    if (!b.hasOwnProperty(key)) {
+      throw new Error(`Translation key ${key} not provided in all translations`);
+    }
+  });
+}
