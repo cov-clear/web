42b57caa659de3c235d2cbb175d3cf2fbf5acec1
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2020-05-11 13:20:00 +0300

    Add id code validation to bulk user creation, refactor tests

--- src/common/idCodeValidation.ts
+++ src/common/idCodeValidation.ts
@@ -3,3 +3,7 @@ import Isikukood from 'isikukood';
 export function validateIdCode(idCode: string): boolean {
   return idCode ? new Isikukood(idCode).validate() : false;
 }
+
+export function validateIdCodes(idCodes: string[]): boolean {
+  return idCodes.every(validateIdCode);
+}
