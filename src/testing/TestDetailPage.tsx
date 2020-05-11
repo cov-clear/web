3b9a7e16058b04924425bbe626b7802b48e3676b
Uku.Tammet <ukutammet@gmail.com>
2020-05-11 22:20:24 +0300

    Use test type from test itself

--- src/testing/TestDetailPage.tsx
+++ src/testing/TestDetailPage.tsx
@@ -4,21 +4,21 @@ import { Heading, Spinner, Text, Divider, Flex, Box } from 'theme-ui';
 import { useTranslations, Message } from 'retranslate';
 
 import { useI18n } from '../common';
-import { useTest, useTestTypes } from '../resources';
+import { useTest } from '../resources';
 import { InterpretationBadge } from './InterpretationBadge';
 
 export const TestDetailPage = () => {
   const { testId } = useParams<{ testId: string }>();
   const { translate } = useTranslations();
-  const { testTypes } = useTestTypes();
   const { test, loading } = useTest(testId);
   const { formatDate } = useI18n();
 
   if (loading || !test) {
     return <Spinner variant="spinner.main" />;
   }
-  const testType = testTypes.find((type) => type.id === test?.testType.id);
-  // TODO: Translate strings coming from the server
+
+  const { testType } = test;
+
   const testResults =
     test?.results && testType
       ? Object.entries(testType.resultsSchema.properties).map(([key, value]) => {
@@ -38,12 +38,15 @@ export const TestDetailPage = () => {
       <Heading as="h1" mb={2}>
         <Message>testDetailPage.heading</Message>
       </Heading>
+
       <Text mb={2}>
         <Message>testDetailPage.date</Message>: {formatDate(new Date(test!.creationTime))}
       </Text>
+
       {test.resultsInterpretations?.map((interpretation, index) => (
         <InterpretationBadge key={index} interpretation={interpretation} mr={2} />
       ))}
+
       {test?.results ? (
         <>
           <Text mt={5}>
@@ -72,6 +75,7 @@ export const TestDetailPage = () => {
           </Flex>
         </>
       ) : null}
+
       {test?.results?.notes ? (
         <>
           <Text mt={5}>
